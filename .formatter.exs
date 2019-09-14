d37423f97a36c87e45adfc84e6263aa9971c6f53
OvermindDL1 <overminddl1@gmail.com>
2019-09-13 22:12:03 -0600

    Better formatter configuration

--- .formatter.exs
+++ .formatter.exs
@@ -1,5 +1,7 @@
 [
   import_deps: [:ecto, :phoenix],
   inputs: ["*.{ex,exs}", "priv/*/seeds.exs", "{config,lib,test}/**/*.{ex,exs}"],
-  subdirectories: ["priv/*/migrations"]
+  subdirectories: ["priv/*/migrations"],
+  single_clause_on_do: true,
+  trailing_comma: true,
 ]
